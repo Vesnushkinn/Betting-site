f9704c803f169d6a48c3151b16efb87d3d203e5a
Kostia <dyachenko.kostia@inbox.ru>
2022-06-24 17:56:38 +0300

    Burger menu and some more changes
    HTML CSS JS

--- filters.js
+++ filters.js
@@ -1,5 +1,7 @@
-const arrowLeft = document.querySelectorAll(".arrow__left-js");
-const arrowRight = document.querySelectorAll(".arrow__right-js");
+const arrowLeft = document.querySelector(".arrow__left-js");
+const arrowRight = document.querySelector(".arrow__right-js");
+const arrowLeftMob = document.querySelector(".arrow__left-jsm");
+const arrowRightMob = document.querySelector(".arrow__right-jsm");
 const bonusesContainer = document.querySelectorAll(".bonuses__flex");
 const bonusFilters = document.querySelector(".other__text-js");
 
@@ -60,17 +62,6 @@ function arrowControl(buttonleft, buttonright, items, buttons) {
         items[activeIndex].classList.remove("active");
         buttons[activeIndex].classList.remove("active__link-orange");
         activeIndex === 0 ? activeIndex = items.length - 1 : activeIndex -= 1;
-        // buttons.forEach((button, buttonIndex) => {
-        //     if (activeIndex === 0) {
-        //         buttons[activeIndex].classList.remove("active__link-orange");
-        //         activeIndex = items.length - 1;
-        //         buttons[buttons.length - 1].classList.add("active__link-orange");
-        //     } else {
-        //         buttons[activeIndex].classList.remove("active__link-orange");
-        //         activeIndex -= 1;
-        //         buttons[activeIndex].classList.add("active__link-orange");
-        //     };
-        // });
         buttons[activeIndex].classList.add("active__link-orange");
         items[activeIndex].classList.add("active");
     })
@@ -78,22 +69,8 @@ function arrowControl(buttonleft, buttonright, items, buttons) {
         items[activeIndex].classList.remove("active");
         buttons[activeIndex].classList.remove("active__link-orange");
         activeIndex === items.length - 1 ? activeIndex = 0 : activeIndex += 1;
-        // buttons.forEach((button, buttonIndex) => {
-        //     if (activeIndex === items.length - 1) {
-        //         buttons[buttons.length - 1].classList.remove("active__link-orange");
-        //         activeIndex = 0
-        //         buttons[activeIndex].classList.add("active__link-orange");
-        //     } else {
-        //         buttons[activeIndex].classList.remove("active__link-orange");
-        //         console.log(activeIndex)
-        //         activeIndex += 1;
-        //         buttonIndex = buttonIndex + 1;
-        //         buttons[buttonIndex].classList.add("active__link-orange");
-        //     };
-        // });
         buttons[activeIndex].classList.add("active__link-orange");
         items[activeIndex].classList.add("active");
-
     });
 };
 
@@ -148,3 +125,56 @@ function bonusesMore(button, items) {
 };
 
 bonusesMore(bonusFilters, bonusesContainer);
+
+const burgerButton = document.querySelectorAll(".menu__button");
+const birgerContainer = document.querySelectorAll(".header__container-js");
+
+function burger(buttons, items) {
+    let activeIndex = 0;
+    buttons[activeIndex].classList.add("active");
+    items[activeIndex].classList.add("active");
+    buttons.forEach((button, buttonIndex) =>
+        button.addEventListener("click", () => {
+            buttons[activeIndex].classList.remove("active");
+            items[activeIndex].classList.remove("active");
+            if (activeIndex === 0) { activeIndex = 1; document.body.style.overflow = "hidden" } else { activeIndex = 0; document.body.style.overflow = "visible" };
+            buttons[activeIndex].classList.add("active");
+            items[activeIndex].classList.add("active");
+        })
+    );
+};
+
+burger(burgerButton, birgerContainer);
+
+function arrowControlOnly(buttonleft, buttonright, items) {
+    if (buttonleft && buttonright && items) {
+        let activeIndex = 0;
+        items[activeIndex].classList.add("active");
+        buttonleft.addEventListener("click", () => {
+            items[activeIndex].classList.remove("active");
+            activeIndex === 0 ? activeIndex = items.length - 1 : activeIndex -= 1;
+            items[activeIndex].classList.add("active");
+        });
+        buttonright.addEventListener("click", () => {
+            items[activeIndex].classList.remove("active");
+            activeIndex === items.length - 1 ? activeIndex = 0 : activeIndex += 1;
+            items[activeIndex].classList.add("active");
+        });
+    }
+};
+
+arrowControlOnly(arrowLeft, arrowRight, bonusesContainer);
+arrowControlOnly(arrowLeftMob, arrowRightMob, bonusesContainer);
+
+const commentButton = document.querySelector(".reply__text");
+const commentForm = document.querySelector(".form2");
+
+function replyFunction(button, form) {
+    if (button && form) {
+        button.addEventListener("click", () => {
+            form.classList.toggle("active");
+        });
+    }
+};
+
+replyFunction(commentButton, commentForm);
